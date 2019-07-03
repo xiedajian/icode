07cd6bc51946d80573eac4521163d89f7004991e
王誉栋 <172265972@qq.com>
2019-07-03 16:09:00 +0800

    xdj

--- chrome扩展/星关系-一键采集/linkeddb/background.js
+++ chrome扩展/星关系-一键采集/linkeddb/background.js
@@ -1,18 +1,17 @@
-
-var isCollecting=false;
+var isCollecting = false;
 
 /**
  *  popup 或者 background 向 content_script主动发送消息
  * @param message {JSON}
  * @param callback  {Function}
  */
-function sendMessageToContentScript(message, callback)
-{
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs)
-    {
-        chrome.tabs.sendMessage(tabs[0].id, message, function(response)
-        {
-            if(callback) callback(response);
+function sendMessageToContentScript(message, callback) {
+    chrome.tabs.query({
+        active: true,
+        currentWindow: true
+    }, function(tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, message, function(response) {
+            if (callback) callback(response);
         });
     });
 }
@@ -28,98 +27,18 @@ chrome.browserAction.onClicked.addListener(function(tab) {
  * @param message
  */
 function showChromeNotifications(message) {
-    chrome.notifications.create( '',{
-        type:'basic',
-        iconUrl:'img/logo.png',
-        title:'提示',
-        message:message
-    },function (nid) {
+    chrome.notifications.create('', {
+        type: 'basic',
+        iconUrl: 'img/logo.png',
+        title: '提示',
+        message: message
+    }, function(nid) {
 
     })
 }
-
-//星关系一键采集
-function oneStepCollect(type,callback) {
-    console.log('正在采集中');
-    if(isCollecting)return;
-    isCollecting=true;
-    //1.获取当前页面的属性信息
-    chrome.tabs.query({
-        active:true,
-        currentWindow:true
-    },function (tabs) {
-        var url=tabs[0].url;    //获取当前页面url
-        console.log('当前页面的url：' +url);
-
-        currentPageAddIframe();
-        // 上传页面url
-        $.ajax({
-            url: "https://api.linkeddb.com/bot/confirm",
-            type:"POST",
-            data:{
-              url:url,
-              type:type
-            },
-            dataType: 'json',
-            async: true,
-            timeout:120000,
-            success: function(data) {
-                if(data.response=='ok'){
-                    var str=type+'-'+data.data.name+'采集成功';
-                    if(callback){
-                        callback();
-                    }
-
-                    // showChromeNotifications(str);
-
-                }else {
-                    if(callback){
-                        callback();
-                    }
-                    showChromeNotifications('失败：'+data.message);
-                }
-                isCollecting=false;
-                console.log('采集完成');
-            },
-            error: function(err) {
-                isCollecting=false;
-                if(callback){
-                    callback();
-                }
-                showChromeNotifications('服务器链接失败，请稍后再试');
-                console.log(err);
-                console.log('采集完成');
-            }
-        });
-    })
-}
-
-// //创建一个鼠标右键菜单
-// chrome.contextMenus.create({
-//     title:'星关系一键采集-电视',
-//     id:'starRelCollectTV',
-//     onclick:function (params) {
-//         console.log(params);
-//         oneStepCollect('tv');
-//     }
-// }, function(){
-//     console.log(chrome.runtime.lastError);
-// });
-//
-// //创建一个鼠标右键菜单
-// chrome.contextMenus.create({
-//     title:'星关系一键采集-电影',
-//     id:'starRelCollectMV',
-//     onclick:function (params) {
-//         console.log(params);
-//         oneStepCollect('mv');
-//     }
-// }, function(){
-//     console.log(chrome.runtime.lastError);
-// });
 
 //复制到剪切板
-var hb_ctws_c2c = function (p_t) {
+var hb_ctws_c2c = function(p_t) {
     var ret_bool = false;
     var ne = $('<textarea></textarea>');
     ne.css('height', '1px').css('width', '1px');
@@ -140,10 +59,10 @@ var hb_ctws_c2c = function (p_t) {
 
 //创建一个鼠标右键菜单 : 纯文本复制
 chrome.contextMenus.create({
-    title:'星关系：纯文本复制',
-    id:'copyText',
-    contexts: ['selection'],    //选中才会出现
-    onclick:function (p_args) {
+    title: '星关系：纯文本复制',
+    id: 'copyText',
+    contexts: ['selection'], //选中才会出现
+    onclick: function(p_args) {
         // console.log(p_args);
         // 方法1：用chrome api获取选中的文字
         // if (typeof p_args.selectionText !== 'undefined') {
@@ -162,11 +81,17 @@ chrome.contextMenus.create({
         // }
 
         //方法2：用content_script的js获取选中内容
-        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-            chrome.tabs.sendMessage(tabs[0].id, {startIframe:false,getSelectionText:true},function(response){
+        chrome.tabs.query({
+            active: true,
+            currentWindow: true
+        }, function(tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, {
+                startIframe: false,
+                getSelectionText: true
+            }, function(response) {
                 console.log(response);
-                if(response.getSelectionText){
-                    var getSelectionText=response.getSelectionText;
+                if (response.getSelectionText) {
+                    var getSelectionText = response.getSelectionText;
                     console.log(getSelectionText);
                     hb_ctws_c2c(getSelectionText);
                 }
@@ -174,20 +99,20 @@ chrome.contextMenus.create({
         });
 
     }
-}, function(){
+}, function() {
     console.log(chrome.runtime.lastError);
 });
 
 
 //创建一个鼠标右键菜单 ：数据采集
 chrome.contextMenus.create({
-    title:'星关系：数据采集',
-    id:'oneStepCollect',
-    onclick:function (params) {
+    title: '星关系：数据采集',
+    id: 'oneStepCollect',
+    onclick: function(params) {
         console.log(params);
         currentPageAddIframe();
     }
-}, function(){
+}, function() {
     console.log(chrome.runtime.lastError);
 });
 
@@ -201,19 +126,22 @@ chrome.contextMenus.onClicked.addListener(function(menuItem) {
 
 //向当前页面注入iframe
 function currentPageAddIframe() {
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, {startIframe:true},function(response){
-        });
+    chrome.tabs.query({
+        active: true,
+        currentWindow: true
+    }, function(tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, {
+            startIframe: true
+        }, function(response) {});
     });
 }
 
 // 监听来自content-script的消息
 //TODO，注意1： content_scripts向popup主动发消息的前提是popup必须打开！否则需要利用background作中转；
 //TODO，注意2：如果background和popup同时监听，那么它们都可以同时收到消息，但是只有一个可以sendResponse，一个先发送了，那么另外一个再发送就无效；
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse)
-{
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     console.log('收到来自content-script的消息：');
-    if(request.greeting){
+    if (request.greeting) {
         showChromeNotifications(request.greeting);
     }
     console.log(request, sender, sendResponse);
--- chrome扩展/星关系-一键采集/linkeddb/iframe.js
+++ chrome扩展/星关系-一键采集/linkeddb/iframe.js
@@ -1,15 +1,14 @@
-
 /**
  * 显示chrome通知信息
  * @param message
  */
 function showChromeNotifications(message) {
-    chrome.notifications.create( '',{
-        type:'basic',
-        iconUrl:'img/logo.png',
-        title:'提示',
-        message:message
-    },function (nid) {
+    chrome.notifications.create('', {
+        type: 'basic',
+        iconUrl: 'img/logo.png',
+        title: '提示',
+        message: message
+    }, function(nid) {
 
     });
 }
@@ -21,38 +20,38 @@ function showChromeNotifications(message) {
  * @param callback
  * @param errback
  */
-function collect(apiUrl,data,callback,errback) {
+function collect(apiUrl, data, callback, errback) {
     chrome.tabs.query({
-        active:true,
-        currentWindow:true
-    },function (tabs) {
-        var url=tabs[0].url;
+        active: true,
+        currentWindow: true
+    }, function(tabs) {
+        var url = tabs[0].url;
         // console.log('当前页面的url：' +url);
-        data.url=url;
+        data.url = url;
 
         // 上传页面url
         $.ajax({
             url: apiUrl,
-            type:"POST",
-            data:data,
+            type: "POST",
+            data: data,
             dataType: 'json',
             async: true,
-            timeout:120000,
+            timeout: 120000,
             success: function(data) {
                 console.log(data);
-                if(data.response=='ok'){
-                    if(callback){
+                if (data.response == 'ok') {
+                    if (callback) {
                         callback(data.data);
                     }
-                }else {
-                    if(errback){
+                } else {
+                    if (errback) {
                         errback();
                     }
-                    showChromeNotifications('采集失败：'+data.message);
+                    showChromeNotifications('采集失败：' + data.message);
                 }
             },
             error: function(err) {
-                if(errback){
+                if (errback) {
                     errback();
                 }
                 showChromeNotifications('服务器链接失败，请稍后再试');
@@ -67,82 +66,153 @@ function collect(apiUrl,data,callback,errback) {
 var app = new Vue({
     el: '#app',
     data: {
-        pageIndex:1,    //当前处于哪个页面      1.首页  2.二级菜单（update，add确认页） 3.结果页
-        isCollecting:false,     //是否正在采集
-        type:'mv',
-        items:[],
-        finalResObj:{},     //结果页展示信息对象
-        title:'二级菜单的标题',
-        message:'最终结果提示信息',
-        btns:[
-            {text:'电视',type:'tv'},
-            {text:'电影',type:'mv'},
-            {text:'人物',type:'person'},
-            {text:'qq音乐歌词',type:'lyric'},
+        pageIndex: 1, //当前处于哪个页面      1.首页  2.二级菜单（update，add确认页） 3.结果页
+        isCollecting: false, //是否正在采集
+        type: 'mv',
+        items: [],
+        finalResObj: {}, //结果页展示信息对象
+        title: '二级菜单的标题',
+        message: '最终结果提示信息',
+        btns: [{
+                text: '电视(百度百科)',
+                type: 'tv'
+            },
+            {
+                text: '电影(百度百科)',
+                type: 'mv'
+            },
+            {
+                text: '人物(百度百科)',
+                type: 'person'
+            },
+            {
+                text: 'qq音乐歌词',
+                type: 'lyric'
+            },
+            {
+                text: '猫眼电影',
+                type: 'my'
+            },
         ],
     },
     methods: {
 
         //采集之前确认已有数据库
-        collect:function (type) {
-            this.type=type;
-            this.isCollecting=true;
-            collect('https://api.linkeddb.com/bot/confirm',{
-                type:type
-            },(data)=>{
-                // console.log(data);
-                this.isCollecting=false;
-                if(data && data.length>0){
-                    //数据库已有数据时，进行数据简介展示，并再次确认操作（更新，添加）
-                    this.pageIndex=2;
-                    this.items=data;
-                    this.title='数据库存在相同数据,请选择替换或者添加';
-                }else {
-                    //没有数据时直接执行添加操作
-                    this.collectAdd(type);
-                }
-            },()=> {
-                this.isCollecting=false;
-                this.pageIndex=1;
-            });
+        collect: function(type) {
+            this.type = type;
+            this.isCollecting = true;
+            // 猫眼电影单独处理
+            if (type === 'my') {
+                this.isCollecting = false;
+                this.collectAdd(type);
+            } else {
+                collect('https://api.linkeddb.com/bot/confirm', {
+                    type: type
+                }, (data) => {
+                    // console.log(data);
+                    this.isCollecting = false;
+                    if (data && data.length > 0) {
+                        //数据库已有数据时，进行数据简介展示，并再次确认操作（更新，添加）
+                        this.pageIndex = 2;
+                        this.items = data;
+                        this.title = '数据库存在相同数据,请选择替换或者添加';
+                    } else {
+                        //没有数据时直接执行添加操作
+                        this.collectAdd(type);
+                    }
+                }, () => {
+                    this.isCollecting = false;
+                    this.pageIndex = 1;
+                });
+            }
+
         },
         //添加新的
-        collectAdd:function(type){
-            type= type || this.type;
+        collectAdd: function(type) {
+            type = type || this.type;
+            var _this = this;
             // console.log('type:'+type);
-            this.isCollecting=true;
-            collect('https://api.linkeddb.com/bot/add',{
-                type:type,
-            },(data)=>{
-                this.isCollecting=false;
-                this.pageIndex=3;
-                this.finalResObj=data;
-                this.message='新增成功';
-            },()=>{
-                this.isCollecting=false;
-                this.pageIndex=3;
-                this.finalResObj={};
-                this.message='添加失败，请稍后重试';
-            });
+            this.isCollecting = true;
+            // 猫眼电影单独处理
+            if (type === 'my') {
+
+                chrome.tabs.query({
+                    active: true,
+                    currentWindow: true
+                }, function(tabs) {
+                    var url = encodeURI(tabs[0].url);
+
+                    console.log('页面的url：' + url);
+                    console.log('location：' + location);
+                    // data.url = url;
+                    var num = url.indexOf("?")
+
+                    var str = url.substr(0, num);
+                    console.log('页面的url-str：' + str);
+                    // 上传页面url
+                    $.ajax({
+                        url: 'https://api.linkeddb.com/down_maoyan/?link=' + str,
+                        type: "GET",
+                        dataType: 'json',
+                        async: true,
+                        timeout: 120000,
+                        success: function(data) {
+                            console.log(data);
+                            this.isCollecting = false;
+                            if (data.response == 'ok') {
+                                _this.isCollecting = false;
+                                _this.pageIndex = 3;
+                                _this.finalResObj = data;
+                                _this.message = '新增成功';
+
+                            } else {
+                                _this.isCollecting = false;
+                                showChromeNotifications('采集失败：' + data.message);
+                            }
+                        },
+                        error: function(err) {
+                            _this.isCollecting = false;
+                            showChromeNotifications('服务器链接失败，请稍后再试');
+                        }
+                    });
+                });
+
+            } else {
+                collect('https://api.linkeddb.com/bot/add', {
+                    type: type,
+                }, (data) => {
+                    this.isCollecting = false;
+                    this.pageIndex = 3;
+                    this.finalResObj = data;
+                    this.message = '新增成功';
+                }, () => {
+                    this.isCollecting = false;
+                    this.pageIndex = 3;
+                    this.finalResObj = {};
+                    this.message = '添加失败，请稍后重试';
+                });
+            }
+
+
         },
         //更有已有数据
-        collectUpdate:function(type,neo_id){
+        collectUpdate: function(type, neo_id) {
             // console.log('type:'+type);
             // console.log('neo_id:'+neo_id);
-            this.isCollecting=true;
-            collect('https://api.linkeddb.com/bot/update',{
-                type:type,
-                neo_id:neo_id
-            },(data)=>{
-                this.isCollecting=false;
-                this.pageIndex=3;
-                this.finalResObj=data;
-                this.message='更新成功';
-            },()=>{
-                this.isCollecting=false;
-                this.pageIndex=3;
-                this.finalResObj={};
-                this.message='更新失败，请稍后重试';
+            this.isCollecting = true;
+            collect('https://api.linkeddb.com/bot/update', {
+                type: type,
+                neo_id: neo_id
+            }, (data) => {
+                this.isCollecting = false;
+                this.pageIndex = 3;
+                this.finalResObj = data;
+                this.message = '更新成功';
+            }, () => {
+                this.isCollecting = false;
+                this.pageIndex = 3;
+                this.finalResObj = {};
+                this.message = '更新失败，请稍后重试';
             });
         },
 
